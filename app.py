2fd12e73bb1766c662d1b15a1576909dfc40893e
asherorr <asher@mailfence.com>
2022-09-25 15:43:18 -0400

    This commit will extend the layout.html file to all pages associated with the navbar, and create the model for portfolio.db

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import render_template, url_for, request
+from models import db, Project, app
 
-app = Flask(__name__)
 
 
 @app.route('/')
@@ -18,6 +18,11 @@ def skills():
     return render_template('skills.html')
 
 
+@app.route('/projects')
+def list_of_projects():
+    return render_template('projects.html')
+
+
 @app.route('/projects/<id>')
 def show_detail_of_project():
     return render_template('detail.html')
@@ -27,14 +32,17 @@ def show_detail_of_project():
 def contact_me():
     return render_template('contact.html')
 
-@app.route('/projects/new')
+
+@app.route('/projects/new_project', methods=['GET', 'POST'])
 def create_new_project():
+    print(request.form)
     return render_template('add_project.html')
 
 
 @app.route('/projects/<id>/edit')
 def edit_or_update_project():
     return render_template('edit_project.html')
+
 
 @app.route('/delete/<id>')
 def delete_project(id):
@@ -45,4 +53,5 @@ def delete_project(id):
     # return redirect(url_for('index'))
     
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True, port=8000, host='127.0.0.1')
---
+++ models.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
+db = SQLAlchemy(app)
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column('Title', db.String())
+    completion_date = db.Column('Completion Date', db.Date)
+    description = db.Column('Description', db.Text)
+    skills = db.Column('Skills', db.String())
+    github_link = db.Column('Github Link', db.String())
+    
+
+    def __repr__(self):
+            return f'''<Project:
+                Name: {self.title}
+                Completion Date: {self.date}
+                Description: {self.description}
+                Skills: {self.skills}
+                Github Link: {self.github_link}'''
