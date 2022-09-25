03ddcfc015153a90392d701e4c5fe552215b7ef4
asherorr <asher@mailfence.com>
2022-09-25 13:48:40 -0400

    This commit will add routes to all pages in the navbar, and hyperlink the navbar elements.

--- app.py
+++ app.py
@@ -2,17 +2,35 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/about')
+def about_me():
+    return render_template('about.html')
+
+
+@app.route('/skills')
+def skills():
+    return render_template('skills.html')
+
+
+@app.route('/projects/<id>')
+def show_detail_of_project():
+    return render_template('detail.html')
+
+
+@app.route('/contact')
+def contact_me():
+    return render_template('contact.html')
 
 @app.route('/projects/new')
 def create_new_project():
     return render_template('add_project.html')
 
-@app.route('/projects/<id>')
-def show_detail_of_project():
-    return render_template('detail.html')
 
 @app.route('/projects/<id>/edit')
 def edit_or_update_project():
